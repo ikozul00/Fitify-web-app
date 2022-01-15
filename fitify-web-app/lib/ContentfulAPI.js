c52b78d48abc0ad3e583c8e5b1f37efffd0af058
imadunic <imadun00@fesb.hr>
2022-01-15 12:03:46 +0100

    Initial blog pagination added

---
+++ fitify-web-app/lib/ContentfulAPI.js
@@ -0,0 +1,76 @@
+import axios from "axios";
+import { Config } from "./Config";
+
+const { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN } = process.env;
+
+const instance = axios.create({
+  baseURL: `https://graphql.contentful.com/content/v1/spaces/${CONTENTFUL_SPACE_ID}`,
+});
+
+const getNumberOfPostsQuery = `query{
+    blogCollection{
+      total
+    }
+  }`;
+
+// Funkcija vraca ukupan broj postova bloga
+export const getNumberOfPosts = async () => {
+  const response = await instance
+    .post(
+      "",
+      {
+        query: getNumberOfPostsQuery,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + CONTENTFUL_ACCESS_TOKEN,
+        },
+      }
+    )
+    .catch((err) => 0);
+
+  const data = response.data.data;
+  return data.blogCollection.total;
+};
+
+// Funkcija vraca clanke bloga po stranicama
+export const getPaginatedPosts = async (page) => {
+  const skipMultiplier = page <= 0 ? 0 : page - 1;
+  const skip = skipMultiplier * Config.pagination.pageSize;
+
+  const query = `query{
+        blogCollection(limit: ${Config.pagination.pageSize}, skip: ${skip}, order: date_DESC) {
+          total
+          items {
+            id
+            title
+            date
+            slug
+            description
+            thumbnailImage{
+                url
+                title
+          }
+          }
+        }
+      }`;
+
+  const response = await instance
+    .post(
+      "",
+      {
+        query: query,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + CONTENTFUL_ACCESS_TOKEN,
+        },
+      }
+    )
+    .catch((err) => ({ total: 0, items: [] })); // U slucaju greske vrati 0 i praznu listu clanaka
+
+  const data = response.data.data;
+  return data.blogCollection;
+};
--- fitify-web-app/pages/blog/index.js
+++ fitify-web-app/pages/blog/index.js
@@ -1,9 +1,11 @@
-import { getPosts } from "../../helper";
 import Link from "next/link";
 import Image from "next/image";
+import { getPaginatedPosts } from "@/lib/ContentfulAPI";
+import { Config } from "@/lib/Config";
 
 const Blog = ({ posts }) => {
   console.log(posts);
+  console.log("Duljina je", posts.length);
   return (
     <div className="w-2/3 mx-auto my-0">
       <h1 className="text-center text-8xl text-gray-800">BLOG</h1>
@@ -19,7 +21,7 @@ const Blog = ({ posts }) => {
               src={post.thumbnailImage.url}
               alt={post.thumbnailImage.title}
               layout="fixed"
-              width={400}
+              width={250}
               height={300}
             />
             {post.description}
@@ -32,9 +34,10 @@ const Blog = ({ posts }) => {
 export default Blog;
 
 export async function getStaticProps() {
-  const posts = await getPosts();
+  const posts = await getPaginatedPosts(1); //Hardcodirano 1, jer se na indeksu uvijek otvara prva stranica
+  const totalPages = Math.ceil(posts.total / Config.pagination.pageSize); // Racunanje broja stranica
 
   return {
-    props: { posts },
+    props: { posts: posts.items, totalPages, currentPage: "1" },
   };
 }
