b1a970779dccc5081af24c71a1a6563eb9ee9cbc
ikozul00 <ivana.kozul5@gmail.com>
2022-01-16 16:45:20 +0100

    Changed header, added active icon for login and shopping bag

--- fitify-web-app/components/navigation/rightNavbar.js
+++ fitify-web-app/components/navigation/rightNavbar.js
@@ -1,15 +1,27 @@
 import Link from "next/link";
+import { useRouter } from 'next/router';
 
 const RightNavbar = () => {
-    
+    const router = useRouter();
+
     return(
         <>
-        <nav className=" bg-fitify-black text-white text-2xl">
+        <nav className=" bg-fitify-black  text-2xl flex text-white">
             <Link href="/login" key="login" passHref>
-                <a className=" px-3"><i class="fas fa-user"></i> </a>
+                <a className=" px-5">
+                    <div className="text-center">
+                        <i className={` ${router.pathname=="/login" ? "text-fitify-green" : "text-white"} fas fa-user`}></i>
+                        <p className=" font-open-sans text-sm">Login</p>
+                    </div>
+                </a>
             </Link>
             <Link href="/shopping_bag" key="shopping_bag" passHref>
-                <a className=" px-3"><i class="fas fa-shopping-cart"></i> </a>
+                <a className=" px-5">
+                    <div>
+                        <i class={` ${router.pathname=="" ? "text-fitify-green" : "text-white"} fas fa-shopping-cart`}></i> 
+                        <p className=" font-open-sans text-sm">Items</p>
+                    </div>
+                </a>
             </Link>
         </nav>
         </>
