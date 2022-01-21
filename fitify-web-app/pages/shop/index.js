5fc69346f6a75423416db5b94134c020f4314caf
imadunic <imadun00@fesb.hr>
2022-01-21 14:58:50 +0100

    HTML text set

--- fitify-web-app/pages/shop/index.js
+++ fitify-web-app/pages/shop/index.js
@@ -44,8 +44,9 @@ const Shop = ({ products, numberOfProducts }) => {
     <main className="full">
       <div className="font-open-sans text-left lg:w-2/3 mx-10 my-10">
         <h1 className="text-5xl fitify-purple my-8">Shop</h1>
-        <p>
-          Define your activewear style with our fashion clothing. Everyday
+        <p
+          dangerouslySetInnerHTML={{
+            __html: `Define your activewear style with our fashion clothing. Everyday
           essentials include relaxed t-shirts, easy-to-wear sweatpants and
           laid-back hoodies to see you comfortably through off-duty weekends.
           Our form-fitting athletic wear is constructed from high-tech materials
@@ -55,8 +56,9 @@ const Shop = ({ products, numberOfProducts }) => {
           the weather. Similarly, moisture-wicking crop tops, t-shirts, leggings
           and shorts will keep you cool as your workout heats up. Whether you’re
           exercising at home, working out in the gym or channelling that
-          athleisure aesthetic, we’ve got something to suit.
-        </p>
+          athleisure aesthetic, we’ve got something to suit.`,
+          }}
+        />
       </div>
       <div>
         <p className="mx-10 my-10">{shownProducts.length} Results</p>
