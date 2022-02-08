4b74ffa0b3657947ae7adbe96d0e0051203d050c
ikozul00 <ivana.kozul5@gmail.com>
2022-02-08 17:14:42 +0100

    Added flex layout to whole page

--- fitify-web-app/modules/layout.js
+++ fitify-web-app/modules/layout.js
@@ -4,9 +4,11 @@ import Header from './header/header'
 const Layout = ({ children}) => {
     return (
         <>
+            <div className='flex flex-col justify-between h-screen'>
             <Header/>
             {children}
             <Footer/>
+            </div>
         </>
     );
 };
