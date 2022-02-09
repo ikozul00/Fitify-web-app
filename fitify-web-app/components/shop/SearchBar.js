347d8eaf6eaf84693cdf8c598cba0a29250d581a
imadunic <imadun00@fesb.hr>
2022-02-09 12:37:13 +0100

    Dodan event za Enter key

--- fitify-web-app/components/shop/SearchBar.js
+++ fitify-web-app/components/shop/SearchBar.js
@@ -8,9 +8,16 @@ const SearchBar = ({ searchQuery }) => {
     searchQuery(query);
   }
 
+  function handleKeyDown(event) {
+    if (event.key === "Enter") searchQuery(query);
+  }
+
   return (
-    <div className="border-black border-2 flex flex-row h-24">
-      <button onClick={handleSearchClick}>
+    <div className="flex border-2 rounded-2xl h-12">
+      <button
+        onClick={handleSearchClick}
+        className="flex items-center justify-center px-4 border-r"
+      >
         <BsSearch />
       </button>
 
@@ -18,7 +25,9 @@ const SearchBar = ({ searchQuery }) => {
         type="text"
         placeholder="Search for products"
         onChange={(event) => setQuery(event.target.value)}
+        onKeyDown={(event) => handleKeyDown(event)}
         value={query}
+        className="px-4 py-2 w-80 rounded-2xl border-0"
       />
     </div>
   );
