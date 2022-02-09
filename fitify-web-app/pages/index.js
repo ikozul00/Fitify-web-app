3668f09b311a0b463590cbf15344eb30bf48a22f
imadunic <imadun00@fesb.hr>
2022-02-09 14:15:26 +0100

    Homepage banner added

--- fitify-web-app/pages/index.js
+++ fitify-web-app/pages/index.js
@@ -1,10 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <p>Text</p>
+    <div className="relative w-full h-2/3">
+      <Image
+        src={require("/public/HomepageImage.png")}
+        alt="Product image"
+        layout="fill"
+        objectFit="cover"
+      />
+      <h1 className="absolute bottom-1/2 left-32 text-6xl my-8 text-white font-semibold">
+        Shop here
+      </h1>
+      <p className="w-1/5 absolute bottom-1/3 left-32 text-4xl my-8 text-white font-semibold">
+        Newest sport fashion and equipment
+      </p>
     </div>
-  )
+  );
 }
