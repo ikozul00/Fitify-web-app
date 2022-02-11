aeffe9e6a626a10c14b4e9ca64b4d4bd4ae853a0
ikozul00 <ivana.kozul5@gmail.com>
2022-02-11 13:05:07 +0100

    Created dropdown for registration and login

--- fitify-web-app/components/navigation/rightNavbar.js
+++ fitify-web-app/components/navigation/rightNavbar.js
@@ -13,17 +13,37 @@ const RightNavbar = ({counter}) => {
         },localStorage.getItem("user"));
     }
 
+    function LogOut(event){
+        event.preventDefault();
+        console.log("evo");
+        localStorage.removeItem("user");
+        router.push("/");
+    }
+
     return(
         <>
         <nav className=" bg-fitify-black  text-2xl flex text-white">
-            <Link href={user  ? `/profile/${user}` : "/login"} key="login" passHref>
+            <div className="relative dropdown px-5">
+                <button>
+                    <i className={`fas fa-user`}></i>
+                </button>
+                <div className="hidden dropdown-content absolute bg-fitify-black right-0 w-32 text-lg text-center">
+                    <Link href={user  ? `/profile/${user}` : "/login"} key="dropdown1" passHref>
+                        <a className="px-10 py-3 whitespace-nowrap hover:bg-fitify-green">{user ? user : "Login"}</a>
+                    </Link>
+                    <Link href={user  ? `/home` : "/registration"} key="dropdown2" passHref>
+                        <a className="px-10 py-3 whitespace-nowrap hover:bg-fitify-green" onClick={user ? (e) => LogOut(e) : () => {}}>{user ? "Log Out" : "Sign Up"}</a>
+                    </Link>
+                </div>
+            </div>
+            {/* <Link href={user  ? `/profile/${user}` : "/login"} key="login" passHref>
                 <a className=" px-4">
                     <div className="text-center hover:opacity-60">
                         <i className={` ${(router.pathname=="/login" || router.pathname.includes(`/profile`)) ? "text-fitify-green" : "text-white"} fas fa-user`}></i>
                         <p className=" font-open-sans text-sm">{user ? user : "Login"}</p>
                     </div>
                 </a>
-            </Link>
+            </Link> */}
             <Link href="/cart" key="cart" passHref>
                 <a className=" px-4">
                     <div className={`hover:opacity-60 ${router.pathname=="/cart" ? "text-fitify-green" : "text-white"}`}>
@@ -31,7 +51,7 @@ const RightNavbar = ({counter}) => {
                             <i className={`fas fa-shopping-cart`}></i> 
                             <p className="px-1">{counter}</p>
                         </div>
-                        <p className=" font-open-sans text-sm">Items</p>
+                        {/* <p className=" font-open-sans text-sm">Items</p> */}
                     </div>
                 </a>
             </Link>
